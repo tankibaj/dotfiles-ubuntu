0c2db02d587cf5087f11c6f5adb1618f2d0f28f6
Naim <ronikh4n@gmail.com>
2020-08-13 12:50:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,15 @@ rm -rf $HOME/.dotfiles
 
 # Repos
 #SpeedTest
-sudo apt-get install gnupg1 apt-transport-https dirmngr
-export INSTALL_KEY=379CE192D401AB61
-export DEB_DISTRO=$(lsb_release -sc)
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
-sudo apt-get update
+#sudo apt-get install gnupg1 apt-transport-https dirmngr
+#export INSTALL_KEY=379CE192D401AB61
+#export DEB_DISTRO=$(lsb_release -sc)
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+#echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+#sudo apt-get update
 
 # Install Apps
-sudo apt install -y zsh curl git jq speedtest
+sudo apt install -y zsh curl git jq speedtest-cli
 
 # ZSH
 sudo usermod -s /usr/bin/zsh $(whoami)
