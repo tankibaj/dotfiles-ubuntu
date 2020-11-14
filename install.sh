ea4d58530bb41f96db5e9894d36385ccd7b92032
Naim <ronikh4n@gmail.com>
2020-11-14 02:30:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,13 @@ echo "Setting up Ubuntu"
 # Remove .dotfiles directory if exist
 rm -rf $HOME/.dotfiles
 
-# Repos
-#SpeedTest
-#sudo apt-get install gnupg1 apt-transport-https dirmngr
-#export INSTALL_KEY=379CE192D401AB61
-#export DEB_DISTRO=$(lsb_release -sc)
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-#echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
-#sudo apt-get update
-
 # Install Apps
-sudo apt install -y zsh curl git jq speedtest-cli
+sudo apt install -y zsh curl git jq zip
+
+# SpeedTest
+wget https://raw.githubusercontent.com/tankibaj/speedtest/master/speedtest
+sudo chmod +x speedtest
+sudo mv speedtest /usr/bin/speedtest
 
 # ZSH
 sudo usermod -s /usr/bin/zsh $(whoami)
