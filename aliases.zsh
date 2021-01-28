08fb8ca823121a936e3f0b4a644a8c761935f88e
Naim <ronikh4n@gmail.com>
2021-01-28 14:58:35 +0100

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -25,9 +25,21 @@ alias size="sudo du --summarize --human-readable"
 alias ports='sudo netstat -tulanp'
 alias killvpn="sudo killall openvpn"
 alias ipt="sudo iptables -nvL"
-alias ipa='bash ~/.dotfiles/functions/ipa.sh'
+#alias ipa='bash ~/.dotfiles/functions/ipa.sh'
 alias ls=' ls -lhF --time-style=long-iso --color=auto'
 alias ls.=' ls -lhFa --time-style=long-iso --color=auto'
+
+# Basic functions
+ipa() {
+    if [[ $(ip -4 addr | grep inet | grep -vEc '127(\.[0-9]{1,3}){3}') -eq 1 ]]; then
+        echo
+        ip -4 addr | grep -w inet | grep -vE '127(\.[0-9]{1,3}){3}' | awk '{ print "\033[0;31m"$7"\033[0m"": ""\033[0;33m"$2"\033[0m"}'
+    else
+        echo
+        ip -4 addr | grep -w inet | grep -vE '127(\.[0-9]{1,3}){3}' | awk '{ print "\033[0;31m"$7"\033[0m"": ""\033[0;33m"$2"\033[0m"}' | nl -s '| '
+    fi
+}
+
 gs() {
     egrep '^\s+\$' GettingStarted.txt | sed -e 's@\$@@'
 }
